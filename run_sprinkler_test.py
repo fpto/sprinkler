9db15ad4aedfe5781c2c740b788e7bb499fe6b8a
fpto <fabricio.puerto@outlook.com>
2018-06-24 17:42:08 -0600

    Simplified script

--- run_sprinkler_test.py
+++ run_sprinkler_test.py
@@ -8,32 +8,35 @@ import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-# Loads configuration file
-def load_config(filename='config'):
-  config = ConfigParser.RawConfigParser()
-  this_dir = os.path.abspath(os.path.dirname(__file__))
-  config.read(this_dir + '/' + filename)
-  if config.has_section('SprinklerConfig'):
-      return {name:val for (name, val) in config.items('SprinklerConfig')}
-  else:
-      print 'Unable to read file %s with section SprinklerConfig' % filename
-      print 'Make sure a file named config lies in the directory %s' % this_dir
-      raise Exception('Unable to find config file')
+api_key = 'qhjhdK4aGlExSQVWA6jbsAhNntab7Wns'
+# City code for Bagalore where it always rain: 188760
+# City code for Tegucigalpa: 188046
+city = 188760
 
-# Method to access accu weather current conditions API and return
-# the value field.  Weather Underground API is not free anymore.
-#
-def get_precip_yesterday(config):
-  API_URL = 'http://dataservice.accuweather.com/currentconditions/v1/{town}/historical/24?apikey={key}&language=en-us&details=true'
-  r = requests.get(API_URL.format(key=config['api_key'],
-                                  town=config['town']))
-  rainfall = None
-  if r.ok:
-    try:
-      rainfall = float(r.json()[0]['PrecipitationSummary']['Past24Hours']['Metric']['Value'])
-    except Exception as ex:
-      rainfall = None
-  return rainfall, r
 
-get_precip_yesterday(config)
-print(rainfall)
+HISTORY_API_URL = 'http://dataservice.accuweather.com/currentconditions/v1/{town}/historical/24?apikey={key}&language=en-us&details=true'
+yesterday_weather = requests.get(HISTORY_API_URL.format(town=city, key=api_key))
+yesterday_rainfall = yesterday_weather.json()[0]['PrecipitationSummary']['Past24Hours']['Metric']['Value']
+print('It rained {yesterday_rainfall} mm of water'.format(yesterday_rainfall=yesterday_rainfall))
+
+FORECAST_API_URL = 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/{town}?apikey={key}&details=true&metric=true'
+todays_weather_forecast = requests.get(FORECAST_API_URL.format(town=city, key=api_key))
+todays_rainfall_forecast = todays_weather_forecast.json()['DailyForecasts'][0]['Day']['Rain']['Value']
+print('Forecast is {todays_rainfall_forecast} mm of rain'.format(todays_rainfall_forecast=todays_rainfall_forecast))
+
+rainfall_48_hours = yesterday_rainfall + yesterday_rainfall
+print('Between yesterday and today expect {rainfall_48_hours} mm of rain'.format(rainfall_48_hours=rainfall_48_hours))
+
+#TODO A log file would be great
+
+# Led light pin
+l = 4
+def ledBlick():
+    	GPIO.output(l, GPIO.HIGH)
+    	time.sleep(0.5)
+    	GPIO.output(l, GPIO.LOW)
+    	time.sleep(0.5)
+
+while True:
+    if todays_rainfall_forecast > 0
+        ledBlick()
