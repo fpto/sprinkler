577346d8b1537f3f93a1e7846c4a0b7e85e48ddf
fpto <fabricio.puerto@outlook.com>
2018-06-24 18:58:32 -0600

    Added standby led

--- run_sprinkler_test.py
+++ run_sprinkler_test.py
@@ -4,6 +4,7 @@ import requests
 import ConfigParser
 import datetime
 import time
+import schedule
 import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -13,23 +14,6 @@ api_key = 'qhjhdK4aGlExSQVWA6jbsAhNntab7Wns'
 # City code for Bagalore where it always rain: 188760
 # City code for Tegucigalpa: 188046
 city = 188760
-
-
-HISTORY_API_URL = 'http://dataservice.accuweather.com/currentconditions/v1/{town}/historical/24?apikey={key}&language=en-us&details=true'
-yesterday_weather = requests.get(HISTORY_API_URL.format(town=city, key=api_key))
-yesterday_rainfall = yesterday_weather.json()[0]['PrecipitationSummary']['Past24Hours']['Metric']['Value']
-print('It rained {yesterday_rainfall} mm of water'.format(yesterday_rainfall=yesterday_rainfall))
-
-FORECAST_API_URL = 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/{town}?apikey={key}&details=true&metric=true'
-todays_weather_forecast = requests.get(FORECAST_API_URL.format(town=city, key=api_key))
-todays_rainfall_forecast = todays_weather_forecast.json()['DailyForecasts'][0]['Day']['Rain']['Value']
-print('Forecast is {todays_rainfall_forecast} mm of rain'.format(todays_rainfall_forecast=todays_rainfall_forecast))
-
-rainfall_48_hours = yesterday_rainfall + yesterday_rainfall
-print('Between yesterday and today expect {rainfall_48_hours} mm of rain'.format(rainfall_48_hours=rainfall_48_hours))
-
-#TODO A log file would be great
-
 
 # Led light pin
 l = 14
@@ -45,11 +29,45 @@ def slowLedBlick():
         GPIO.output(l, GPIO.LOW)
         time.sleep(1)
 
-# uncomment line below for testing lights
+def job():
+    try:
+    HISTORY_API_URL = 'http://dataservice.accuweather.com/currentconditions/v1/{town}/historical/24?apikey={key}&language=en-us&details=true'
+    yesterday_weather = requests.get(HISTORY_API_URL.format(town=city, key=api_key))
+    yesterday_rainfall = yesterday_weather.json()[0]['PrecipitationSummary']['Past24Hours']['Metric']['Value']
+    print('It rained {yesterday_rainfall} mm of water'.format(yesterday_rainfall=yesterday_rainfall))
 
-while True:
+    FORECAST_API_URL = 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/{town}?apikey={key}&details=true&metric=true'
+    todays_weather_forecast = requests.get(FORECAST_API_URL.format(town=city, key=api_key))
+    todays_rainfall_forecast = todays_weather_forecast.json()['DailyForecasts'][0]['Day']['Rain']['Value']
+    print('Forecast is {todays_rainfall_forecast} mm of rain'.format(todays_rainfall_forecast=todays_rainfall_forecast))
+
+    rainfall_48_hours = yesterday_rainfall + yesterday_rainfall
+    print('Between yesterday and today expect {rainfall_48_hours} mm of rain'.format(rainfall_48_hours=rainfall_48_hours))
+
     if todays_rainfall_forecast > 0:
         quickLedBlick()
     else:
-	slowLedBlick()
+	    slowLedBlick()
 
+    except KeyboardInterrupt:
+        print " Quit"
+        GPIO.cleanup()
+
+l2 = 18
+def standbyBlick():
+        GPIO.output(l2, GPIO.HIGH)
+        time.sleep(0.25)
+        GPIO.output(l, GPIO.LOW)
+        time.sleep(2)
+
+
+schedule.every().minute.do(job)
+
+#TODO A log file would be great
+
+
+# uncomment line below for testing lights
+
+while True:
+    schedule.run_pending()
+    standbyBlick()
